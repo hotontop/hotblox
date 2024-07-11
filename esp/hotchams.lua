2e1a3c8a316bc4cbc78f587b61d9e2a9e899f1be
retardcoder <89077627+retardcoder@users.noreply.github.com>
2024-07-11 06:47:15 +0300

    Create hotchams.lua

---
+++ esp/hotchams.lua
@@ -0,0 +1,77 @@
+paint ("                                                   ")
+paint (" _    _       _    _____ _                         ")
+paint ("| |  | |     | |  / ____| |                        ")
+paint ("| |__| | ___ | |_| |    | |__   __ _ _ __ ___  ___ ")
+paint ("|  __  |/ _ \| __| |    | '_ \ / _` | '_ ` _ \/ __|")
+paint ("| |  | | (_) | |_| |____| | | | (_| | | | | | \__ \")
+paint ("|_|  |_|\___/ \__|\_____|_| |_|\__,_|_| |_| |_|___/")
+paint ("                                                   ")
+
+
+  local client = game.Players.LocalPlayer
+local players = game:GetService("Players")
+local rs = game:GetService("RunService")
+
+local function clonePart(part, model, character)
+   if part:IsA("BasePart") then
+local head = character:WaitForChild("Head")
+local clone = part:Clone()
+for i, v in ipairs(clone:GetChildren()) do
+if not v:IsA("SpecialMesh") then
+v:Destroy()
+continue
+end
+v.TextureId = ""
+end
+clone.Color = Color3.fromRGB(0, 255, 0)
+pcall(function()clone.Size = clone.Size * 0.7 clone.CanCollide = false end)
+clone.Parent = model
+rs.RenderStepped:connect(function()
+if head:IsDescendantOf(workspace) then
+clone.CFrame = part.CFrame
+clone.Size = part.Size
+else
+model:Destroy()
+return
+end
+end)
+end
+end
+local function chams(character)
+   local model = Instance.new("Model")
+   model.Name = character.Name
+   model.Parent = workspace
+   local cham = Instance.new("Highlight",model)
+   cham.Name = "cham"
+   cham.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop  
+   cham.OutlineColor = Color3.fromRGB(0,0,0)
+   cham.FillColor = Color3.fromRGB(40,119,208)
+   cham.FillTransparency = 0.4
+   cham.OutlineTransparency = 0.1
+   local new = cham:Clone()
+   new.Parent = character
+   new.FillColor = Color3.fromRGB(176,221,22)
+   new.DepthMode = Enum.HighlightDepthMode.Occluded  
+   for i,v in ipairs(character:GetChildren()) do
+-- if character has a head then
+       if character:FindFirstChild("Head") then
+           clonePart(v, model, character)
+       end
+   end
+end
+for i,v in ipairs(players:GetPlayers()) do
+   if v~= client then
+       if v.Character then
+           chams(v.Character)
+       end
+   end
+   v.CharacterAdded:connect(function()wait(0.1)chams(v.Character)end)
+end
+players.PlayerAdded:connect(function()
+   if v~= client then
+       if v.Character then
+           chams(v.Character)
+       end
+   end
+   v.CharacterAdded:connect(function()wait(0.1)chams(v.Character)end)
+end)
