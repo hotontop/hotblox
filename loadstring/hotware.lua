474f26fa785ff9798a2d3ba3acc7c8b6808fcb19
hot <89077627+hotontop@users.noreply.github.com>
2024-07-14 11:08:31 +0300

    Update hotware.lua

--- loadstring/hotware.lua
+++ loadstring/hotware.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/retardcoder/hotbloxai/main/other/hotware.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hotontop/hotblox/main/other/hotware.lua"))()
